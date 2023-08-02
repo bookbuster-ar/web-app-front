fccf831db4b43c75e8e60776771952120781285e
REValdiviezo <ezequielrvaldiviezo@gmail.com>
2023-08-02 12:46:00 -0300

    Estructura del Nav

--- src/components/navBar/NavBar.jsx
+++ src/components/navBar/NavBar.jsx
@@ -1,8 +1,19 @@
 import SearchBar from './SearchBar';
 
+
 const NavBar = () => {
   return (
     <div>
+      <div>
+        <img src='' alt='Logo' />
+        <button>Explorá nuestros libros</button>
+        <button>¿Por qué suscribirme?</button>
+        <button>Vendé tus libros</button>
+      </div>
+      <div>
+        <button>Comprar suscripción</button>
+        <button>Iniciar sesión</button>
+      </div>
       <div>
         <SearchBar />
       </div>
