b0c05891289e0beb1987ba1ec537f0f757a87472
REValdiviezo <ezequielrvaldiviezo@gmail.com>
2023-08-02 18:56:28 -0300

    Agregando estilos al NavBar

--- src/components/navBar/SearchBar.jsx
+++ src/components/navBar/SearchBar.jsx
@@ -1,8 +1,9 @@
 const SearchBar = () => {
   return (
-    <div>
-      <input type='text' />
-      <button></button>
+    <div className='space-x-2 flex items-center ml-15'>
+      <input type='text' placeholder="Buscá tu próximo libro" 
+      className='text-black text-base rounded-xl pl-2 w-72 bg-transparent border border-red-600 outline-none'/>
+      <button>Buscar</button>
     </div>
   );
 };
