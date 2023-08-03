fd739a0cd0b0a0b88efcbecdf7de9966d20b7699
Lucas Lopez <lucaslopezmaxi7@gmail.com>
2023-08-03 17:06:52 -0300

    Cambiamos los estilos

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    screens: {
+      sm: '640px',
+      md: '768px',
+      lg: '1024px',
+      xl: '1280px',
+    },
+    fontFamily: {
+      sans: ['Montserrat', 'sans-serif'],
+    },
+    extend: {
+      colors: {
+        primary: '#020617',
+        secondary: '#0f172a',
+        secondaryBorder: '#1e293b',
+      },
+    },
   },
   plugins: [],
-}
-
+};
