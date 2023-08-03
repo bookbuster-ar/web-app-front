5bc867cf5415f40cd754a230eed378f6ede31f9a
Millifboto <milifboto@gmail.com>
2023-08-03 07:57:39 -0300

    Creamos el store y configuramos la slice books

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App.jsx';
+import { Provider } from 'react-redux';
+import store from './store/store.js';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  </BrowserRouter>
+  <Provider store={store}>
+    <BrowserRouter>
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    </BrowserRouter>
+  </Provider>
 );
---
+++ src/store/books/booksSlice.js
@@ -0,0 +1,21 @@
+import { createSlice } from '@reduxjs/toolkit';
+import axios from 'axios';
+
+const initialState = {
+  books: [],
+  status: 'idle', // loading, failed, succedded
+  error: null,
+};
+
+const booksSlice = createSlice({
+  name: 'books',
+  initialState,
+  reducers: {},
+});
+
+export const selectAllBooks = (state) => state.books.books;
+export const selectStatus = (state) => state.books.status;
+export const selectError = (state) => state.books.error;
+
+// export const {actions jeje} = booksSlice.actions;
+export default booksSlice.reducer;
