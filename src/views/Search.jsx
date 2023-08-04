03726532b0317bd2bdeb22a46ba626ea03aabade
Millifboto <milifboto@gmail.com>
2023-08-04 11:32:27 -0300

    Cambios

--- src/views/Search.jsx
+++ src/views/Search.jsx
@@ -1,5 +1,7 @@
 import SearchBar from '../components/SearchBar';
 
+const genres = [ "Bienestar y salud" , "Ciencia", "Feminismos y LGBTIQ", "Historia y Política", "Infatil-Niños", "Música", "Narrava", "No ficción", "Pensamientos y Filosofía", "Poesía", "Relatos breves", "Teatro"]
+
 const Search = () => {
   return (
     <div className='bg-white h-screen flex justify-center space-y-12'>
