2a741037ec24ed003812515af5cd66183db1ae66
Lucas Lopez <lucaslopezmaxi7@gmail.com>
2023-08-08 23:53:26 -0300

    Editorial dinamica con un mock

--- src/icons/Delete.jsx
+++ src/icons/Delete.jsx
@@ -1,5 +1,3 @@
-import * as React from 'react';
-
 function Delete(props) {
   return (
     <svg
---
+++ src/mock/editoriales.js
@@ -0,0 +1,42 @@
+export const editoriales = [
+  {
+    id: '1d2ef632-e404-4417-b93f-e130b95d2646',
+    name: 'Libros Magníficos',
+  },
+  {
+    id: '47ab97ac-eb0c-46ec-a1c3-55e0a7de1fb0',
+    name: 'Escritura Brillante',
+  },
+  {
+    id: '3bb7c4f6-1dc5-429a-a635-c557e025146e',
+    name: 'Páginas Doradas',
+  },
+  {
+    id: 'f143b39e-70be-4838-a185-fd76c7177c4d',
+    name: 'Letras Preciosas',
+  },
+  {
+    id: '0bf4d2c5-fe17-4b62-a900-6d9385492427',
+    name: 'Mundos Imaginarios',
+  },
+  {
+    id: 'ebed41f1-54f4-4047-beb8-a573181d4a30',
+    name: 'Aventuras Literarias',
+  },
+  {
+    id: '69f5892f-7a2c-40ab-82d2-70c4bd981d3f',
+    name: 'Poesía Inspiradora',
+  },
+  {
+    id: 'bdf58952-0600-4f70-a843-4eab2dca7bea',
+    name: 'Sabiduría de Tinta',
+  },
+  {
+    id: '75626abb-2bdd-45e4-b67b-337fe5698ecf',
+    name: 'Sueños Literarios',
+  },
+  {
+    id: '0a5cfffc-3d28-4766-bf46-cf2634da7be6',
+    name: 'Fantasía de Papel',
+  },
+];
