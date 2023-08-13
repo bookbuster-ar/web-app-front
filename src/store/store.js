b7631361233b2c445d4505fe3a5a241bd12d7ee9
Millifboto <milifboto@gmail.com>
2023-08-13 09:19:13 -0300

    Creación de componentes relacionados a la entidad Review y su formulario

--- src/store/store.js
+++ src/store/store.js
@@ -3,13 +3,15 @@ import bookReducer from './books/bookSlice';
 import bookshelvesReducer from './books/bookshelvesSlice';
 import userReducer from './user/userSlice';
 import authReducer from './user/authSlice';
+import reviewsReducer from './reviews/reviewsSlice';
 
 const store = configureStore({
   reducer: {
     auth: authReducer,
     books: bookReducer,
     bookshelves: bookshelvesReducer,
-    user: userReducer,
+    users: userReducer,
+    reviews: reviewsReducer,
   },
 });
 
