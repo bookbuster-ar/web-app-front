3eabb521892df3fddd33f523ace880a24e48123c
Lucas Lopez <lucaslopezmaxi7@gmail.com>
2023-08-18 11:20:02 -0300

    Route Narrativa hardcodeada y 404 mas bonito

--- src/views/Library.jsx
+++ src/views/Library.jsx
@@ -40,22 +40,24 @@ const Library = () => {
       </h1>
       <div className='h-[800px] w-[900px]'>
         <div className='grid h-full grid-cols-12 grid-rows-6 relative'>
-          <div className='bg-orangebook relative col-span-4 row-span-2 text-4xl font-roboto text-white font-black cursor-pointer overflow-hidden group hover:bg-orange-200'>
-            <div className='relative transition-all duration-500 h-full ease-in left-5'>
-              <div className='absolute bottom-2 leading-10 font-roboto duration-500 group-hover:bottom-24 group-hover:text-bluebook'>
-                NARRATIVAS
-              </div>
-              <div className='absolute bottom-2 transition-opacity opacity-0 group-hover:opacity-100 max-w-[250px] leading-4 duration-300 text-[10px] text-bluebook'>
-                Se trata de un género literario que presenta una serie de hechos
-                reales o ficticios. Se caracteriza por ser una narración
-                extensa, tener una trama compleja en la que el narrador se vale
-                de la descripción, diálogos o monólogos.
-              </div>
-              <div className='absolute opacity-0 group-hover:opacity-100 transition-opacity duration-500 right-8 top-4 w-10 h-10'>
-                <img src={RedMark} alt='Marcador Rojo' />
+          <Link to='genre/fc94da93-75ff-4e12-a9cd-b61333126dce'>
+            <div className='bg-orangebook relative col-span-4 row-span-2 text-4xl font-roboto text-white font-black cursor-pointer overflow-hidden group hover:bg-orange-200'>
+              <div className='relative transition-all duration-500 h-full ease-in left-5'>
+                <div className='absolute bottom-2 leading-10 font-roboto duration-500 group-hover:bottom-24 group-hover:text-bluebook'>
+                  NARRATIVAS
+                </div>
+                <div className='absolute bottom-2 transition-opacity opacity-0 group-hover:opacity-100 max-w-[250px] leading-4 duration-300 text-[10px] text-bluebook'>
+                  Se trata de un género literario que presenta una serie de
+                  hechos reales o ficticios. Se caracteriza por ser una
+                  narración extensa, tener una trama compleja en la que el
+                  narrador se vale de la descripción, diálogos o monólogos.
+                </div>
+                <div className='absolute opacity-0 group-hover:opacity-100 transition-opacity duration-500 right-8 top-4 w-10 h-10'>
+                  <img src={RedMark} alt='Marcador Rojo' />
+                </div>
               </div>
             </div>
-          </div>
+          </Link>
 
           <div className='bg-redbook col-span-3 row-span-3 text-2xl text-yellowbook font-sans font-black cursor-pointer'>
             <span className='absolute top-56 inset-x-80 font-roboto'>
