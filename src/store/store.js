813e1776c45da7f624f017040b119706a2d17e00
Fede Risetti <115749116+risettifede@users.noreply.github.com>
2023-08-22 23:05:58 -0300

    Carrito comenzado pero falta terminarlo

--- src/store/store.js
+++ src/store/store.js
@@ -14,6 +14,7 @@ import reviewsReducer from './reviews/reviewsSlice';
 import paymentReducer from './payment/paymentSlice';
 import quotesReducer from './quotes/quotesSlice';
 import booksForRentReducer from './books/booksForRentSlice';
+import booksForCartReducer from './shopping/shoppingSlice';
 
 const persistConfig = {
   key: 'root',
@@ -31,6 +32,7 @@ const rootReducer = combineReducers({
   payment: paymentReducer,
   quotes: quotesReducer,
   booksForRent: booksForRentReducer,
+  booksForCart: booksForCartReducer,
 });
 
 const persistedReducer = persistReducer(persistConfig, rootReducer);
