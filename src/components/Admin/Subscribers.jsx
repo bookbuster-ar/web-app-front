fded686f0b0c4de66aafd10aaf5c302930079fb7
Lucas Lopez <lucaslopezmaxi7@gmail.com>
2023-08-23 23:04:19 -0300

    avances en admin

--- src/components/Admin/Subscribers.jsx
+++ src/components/Admin/Subscribers.jsx
@@ -5,7 +5,7 @@ import { getSubscriptions, selectSubscriptions } from '../../store/user/adminSli
 
 const Subscribers = () => {
   const dispatch = useDispatch()
-  const subscribersList = useSelector(selectSubscriptions)
+  const {subscriptionCount, users} = useSelector(selectSubscriptions)
 
   useEffect(() => {
     dispatch(getSubscriptions())
@@ -16,16 +16,15 @@ const Subscribers = () => {
 
   return (
     <div className='text-center'>
-      <h1>Suscriptores totales: {subscribersList.subscriptionCount || 0}</h1>
-      {/* 
-      <ul className='grid grid-cols-12 gap-5 w-full font-bold sticky top-0 bg-white py-4'>
+      <h1 className='bg-bluebook w-60 mx-auto text-white rounded-lg py-2 px-4'>Suscriptores</h1>
+      <h1 className='m-4 p-4 w-80 mx-auto font-bold text-bluebook '>Suscriptores totales: <span className='bg-bluebook text-white py-1 px-2 rounded-lg'>{subscriptionCount || 0}</span></h1>
+      <ul className='hidden lg:bg-bluebook text-white rounded-lg lg:grid grid-cols-6 gap-5 w-full font-bold sticky top-0 bg-white py-4'>
         <li>Foto</li>
         <li className='col-span-2'>Nombre</li>
         <li className='col-span-3 text-center'>Email</li>
-        <li className='col-span-4'>Razón de ban</li>
       </ul>
       <ul className='grid grid-cols-1 gap-5 w-full'>
-        {subscribersList.map((user) => (
+        {users.map((user) => (
           <li
             className={`grid grid-cols-12 items-center gap-x-5 border-b-2 rounded-lg `}
             key={user.id}
@@ -33,16 +32,15 @@ const Subscribers = () => {
             <img
               src={user.image || imageDefault}
               alt={user.name}
-              className='mx-auto p-4 object-cover rounded-full'
+              className='mx-auto h-16 w-16 md:h-20 md:w-20 lg:h-24 lg:w-24 col-span-4 md:col-span-2 lg:col-span-2 p-4 object-cover rounded-full'
             />
-            <h3 className='font-semibold text-md col-span-2'>
+            <h3 className='font-semibold text-md col-span-8 md:col-span-3 lg:col-span-4'>
               {user.name} {user.last_name}
             </h3>
-            <h3 className='font-semibold text-sm col-span-3'>{user.email}</h3>
-            <p className='text-md col-span-4'>Si llegamos le metemos la razon</p>
+            <h3 className='font-semibold text-sm col-span-12 md:col-span-6'>{user.email}</h3>
           </li>
         ))}
-      </ul> */}
+      </ul>
     </div>
   )
 }
--- src/components/Admin/UserList.jsx
+++ src/components/Admin/UserList.jsx
@@ -35,23 +35,22 @@ export function ListOfUsers({ users, toggle }) {
     'https://static.vecteezy.com/system/resources/previews/008/442/086/original/illustration-of-human-icon-user-symbol-icon-modern-design-on-blank-background-free-vector.jpg';
 
   return (
-    <div className='text-center'>
-      <ul className='grid grid-cols-12 gap-5 w-full font-bold sticky top-0 bg-white py-4'>
-        <li>Foto</li>
+    <div className='text-center flex flex-col lg:inline'>
+      <ul className='hidden lg:grid lg:bg-bluebook text-white  rounded-lg grid-cols-12 gap-5 w-full font-bold sticky -top-2 bg-white py-4'>
+        <li className='lg:col-span-2'>Foto</li>
         <li className='col-span-2'>Nombre</li>
-        <li>Suscriptor</li>
-        <li className='col-span-3 text-center'>Email</li>
-        <li className='text-center'>
+        <li>Suscrip.</li>
+        <li className='col-span-3 text-center xl:col-span-2'>Email</li>
+        <li className='text-center -mb-3 -mt-3'>
           Miembro <br />
           desde
         </li>
-        <li className='col-span-2'>Banear</li>
-        <li className='col-span-1'>Razón de ban</li>
+        <li className='col-span-3 xl:col-span-4'>Banear/Razon</li>
       </ul>
       <ul className='grid grid-cols-1 gap-5 w-full'>
         {users.map((user) => (
           <li
-            className={`grid grid-cols-12 items-center gap-x-5 border-b-2 rounded-lg ${
+            className={`grid grid-cols-12 items-center gap-x-5 border-b-2 rounded-lg p-2 ${
               toggle ? 'bg-blue-100' : ''
             }`}
             key={user.id}
@@ -59,17 +58,17 @@ export function ListOfUsers({ users, toggle }) {
             <img
               src={user.image || imageDefault}
               alt={user.name}
-              className='mx-auto p-4 object-cover rounded-full'
+              className='mx-auto col-span-4 md:col-span-2 md:h-20 md:w-20 lg:h-24 lg:w-24 lg:col-span-2 p-4 object-cover rounded-full'
             />
-            <h3 className='font-semibold text-md col-span-2'>
+            <h3 className='font-semibold text-md col-span-4 md:col-span-3 lg:col-span-2'>
               {user.name} {user.last_name}
             </h3>
-            <p className='text-md'>{user.subscription ? 'Si' : 'No'}</p>
-            <h3 className='font-semibold text-sm col-span-3'>{user.email}</h3>
-            <p className='text-xs'>{user.date_of_register}</p>
-            <div className='flex col-span-2 justify-center'>
+            <p className='text-md col-span-4 md:col-span-2 lg:col-span-1'>{user.subscription ? 'Si' : 'No'}</p>
+            <h3 className='font-semibold text-sm lg:text-xs col-span-6 md:col-span-5 lg:col-span-3 xl:col-span-2'>{user.email}</h3>
+            <p className='text-xs col-span-6 md:col-span-2 lg:col-span-1 xl:col-span-1'>{user.date_of_register}</p>
+            <div className='flex col-span-6 md:col-span-4 lg:col-span-3 xl:col-span-2 justify-center'>
               <input
-                className='h-8 rounded-l-xl w-20'
+                className='rounded-l-xl h-5 md:h-6 xl:h-8 text-xs xl:text-md  w-16 xl:w-20'
                 type='number'
                 name='banear'
                 id={`banear-${user.id}`} // Para evitar duplicidad de ids
@@ -82,7 +81,7 @@ export function ListOfUsers({ users, toggle }) {
               <button
                 onClick={() => handleBanned(user.id)}
                 disabled={!durations[user.id] || durations[user.id] <= 0}
-                className={`text-white text-sm rounded-r-xl py-1 px-2 ${
+                className={`text-white rounded-r-xl h-5 md:h-6 xl:h-8 text-xs xl:text-md w-14 xl:w-20 px-2 ${
                   !durations[user.id] || durations[user.id] <= 0
                     ? 'bg-gray-500'
                     : 'bg-red-500 hover:bg-red-600'
@@ -91,9 +90,11 @@ export function ListOfUsers({ users, toggle }) {
                 Banear
               </button>
             </div>
+            <div className='lg:col-span-9 xl:hidden'></div>
             <div>
               <input
-                className='h-8 w-40'
+                className='h-5 md:h-6 xl:h-8 align-middle w-24 md:w-40 -ml-4 lg:ml-6 xl:ml-0 lg:w-40 xl:w-40 col-span-6 md:col-span-4 lg:col-span-4 text-[8px] md:text-xs xl:text-sm rounded-md'
+                placeholder='Razón de baneo..'
                 value={reasons[user.id] || ''}
                 onChange={(event) => handleReasons(user.id, event)}
                 type='text'
