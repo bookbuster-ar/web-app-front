3a6570418f17a31bfcca1d9c8384c9498ff07e48
Fede Risetti <115749116+risettifede@users.noreply.github.com>
2023-08-24 09:01:15 -0300

    About

--- src/views/Footer.jsx
+++ src/views/Footer.jsx
@@ -6,16 +6,16 @@ const Footer = () => {
   return (
     <footer className='bg-bluebook h-[50px] xl:h-24 no-scroll-x'>
       <div className='h-full flex justify-center md:justify-between items-center mx-4 md:mx-6 xl:mx-8'>
-        <div className='w-10 h-10 xl:w-16 xl:h-16 hover:scale-110 transition-all duration-300 hover:bg-opacity-20 hover:bg-violet-600'>
-          <Link to='/about'>
+        <Link to='/about'>
+          <div className='w-10 h-10 xl:w-16 xl:h-16 hover:scale-110 transition-all duration-300 hover:bg-opacity-20 hover:bg-violet-600'>
             <img src={FooterBook} alt='Logo de Book Buster' />
-          </Link>
-        </div>
-        <div className='hidden md:inline md:w-6 md:h-6 xl:h-10 xl:w-10 hover:scale-110 transition-all duration-300 hover:bg-opacity-20 hover:bg-violet-600'>
-          <Link to='/about'>
+          </div>
+        </Link>
+        <Link to='/about'>
+          <div className='hidden md:inline md:w-6 md:h-6 xl:h-10 xl:w-10 hover:scale-110 transition-all duration-300 hover:bg-opacity-20 hover:bg-violet-600'>
             <img src={FooterSave} alt='Logo de guardado amarillo' />
-          </Link>
-        </div>
+          </div>
+        </Link>
       </div>
     </footer>
   );
